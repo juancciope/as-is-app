99e706ad6ef8bc21b34457c60807c9e94eb59135
Juan Garcia <juan@blo7.com>
2025-07-07 14:51:03 -0500

    Inline cn function in UI components to avoid import path issues

--- foreclosure-scraper/components/ui/label.tsx
+++ foreclosure-scraper/components/ui/label.tsx
@@ -1,7 +1,12 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "../../lib/utils"
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
--- foreclosure-scraper/components/ui/switch.tsx
+++ foreclosure-scraper/components/ui/switch.tsx
@@ -1,6 +1,11 @@
 import * as React from "react"
 import * as SwitchPrimitives from "@radix-ui/react-switch"
-import { cn } from "../../lib/utils"
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
